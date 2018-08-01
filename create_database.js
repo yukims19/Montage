662dc1c6b562fc7a542b6169d83ff2f51438b8cc
Youxi Li <yol107@ucsd.edu>
2018-08-01 13:43:53 -0700

    Cleaned code for table initialization

--- create_database.js
+++ create_database.js
@@ -3,35 +3,17 @@ const db = new sqlite3.Database("montage.db");
 db.serialize(() => {
   //customers table
   db.run(
-    "CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, url TEXT, twitter TEXT, github TEXT, AvatarUrl TEXT, location TEXT, email TEXT, CONSTRAINT con_emp_name_unique UNIQUE (name, url, twitter, github, AvatarUrl, location, email));"
-  );
-  /*
-  db.run(
-    "INSERT INTO people VALUES (1,'Youxi Li', 'http://yosili.com', 'yukims19', 'yukims19', 'htcustomerstps://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png', 'San Diego, CA', 'yukims19@gmail.com')"
-  );
-  db.run(
-    "INSERT INTO people VALUES (2,'Sean Grove', 'http://www.riseos.com', 'sgrove', 'sgrove', 'https://pbs.twimg.com/profile_images/913444398133735427/7zjUK6pp_normal.jpg', 'San Francisco, CA', 'sgrove@gmail.com')"
+    "CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, url TEXT, twitter TEXT, github TEXT, AvatarUrl TEXT, location TEXT, email TEXT, producthunt_id TEXT, CONSTRAINT con_emp_name_unique UNIQUE (name, url, twitter, github, AvatarUrl, location, email));"
   );
 
-  console.log("successfully created the users_to_pets table in pets.db");
-  db.each("SELECT * FROM people", (err, row) => {
-    console.log(row.name + ": " + row.url + " - " + row.twitter + row.github);
-  });*/
   //votes table
   db.run("CREATE TABLE votes (uid NUM, pid NUM)");
-  db.run("INSERT INTO votes VALUES (1 , 1)");
-  db.run("INSERT INTO votes VALUES (2 , 2)");
-  db.run("INSERT INTO votes VALUES (2 , 3)");
-
-  db.each("SELECT * FROM votes", (err, row) => {
-    console.log(row.pid + "," + row.uid);
-  });
 
   //posts table
-  db.run("create table posts(id INTEGER PRIMARY KEY,title TEXT)");
   db.run(
-    "    insert into posts (title) values ('example1'), ('example2'), ('example3') "
+    "create table posts(id INTEGER PRIMARY KEY AUTOINCREMENT, slug TEXT, cursor TEXT, CONSTRAINT unique_slug UNIQUE(slug))"
   );
+  console.log("successfully created the tables");
 });
 
 db.close();
