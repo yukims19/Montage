8a42b3a387e4af35640c30443886a1ce47f30b31
Youxi Li <yol107@ucsd.edu>
2018-08-01 11:51:45 -0700

    Added constraint to people table

--- create_database.js
+++ create_database.js
@@ -3,19 +3,20 @@ const db = new sqlite3.Database("montage.db");
 db.serialize(() => {
   //customers table
   db.run(
-    "CREATE TABLE people (id INTEGER PRIMARY KEY, name TEXT, url TEXT, twitter TEXT, github TEXT, AvatarUrl TEXT, location TEXT, email TEXT)"
+    "CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, url TEXT, twitter TEXT, github TEXT, AvatarUrl TEXT, location TEXT, email TEXT, CONSTRAINT con_emp_name_unique UNIQUE (name, url, twitter, github, AvatarUrl, location, email));"
+  );
+  /*
+  db.run(
+    "INSERT INTO people VALUES (1,'Youxi Li', 'http://yosili.com', 'yukims19', 'yukims19', 'htcustomerstps://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png', 'San Diego, CA', 'yukims19@gmail.com')"
   );
   db.run(
-    "INSERT INTO customers VALUES (1,'Youxi Li', 'http://yosili.com', 'yukims19', 'yukims19', 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png', 'San Diego, CA', 'yukims19@gmail.com')"
-  );
-  db.run(
-    "INSERT INTO customers VALUES (2,'Sean Grove', 'http://www.riseos.com', 'sgrove', 'sgrove', 'https://pbs.twimg.com/profile_images/913444398133735427/7zjUK6pp_normal.jpg', 'San Francisco, CA', 'sgrove@gmail.com')"
+    "INSERT INTO people VALUES (2,'Sean Grove', 'http://www.riseos.com', 'sgrove', 'sgrove', 'https://pbs.twimg.com/profile_images/913444398133735427/7zjUK6pp_normal.jpg', 'San Francisco, CA', 'sgrove@gmail.com')"
   );
 
   console.log("successfully created the users_to_pets table in pets.db");
-  db.each("SELECT * FROM customers", (err, row) => {
+  db.each("SELECT * FROM people", (err, row) => {
     console.log(row.name + ": " + row.url + " - " + row.twitter + row.github);
-  });
+  });*/
   //votes table
   db.run("CREATE TABLE votes (uid NUM, pid NUM)");
   db.run("INSERT INTO votes VALUES (1 , 1)");
