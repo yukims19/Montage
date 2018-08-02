c00d2085fc8d25f8a64a1b53bdab757cd7a09c3a
Youxi Li <yol107@ucsd.edu>
2018-08-02 14:30:33 -0700

    Convert SQLite database to PostgreSQL (#4)
    - Convert database to PostgreSQL
    - Accommodate UI for new database

--- create_database.js
+++ create_database.js
@@ -1,4 +1,31 @@
-const sqlite3 = require("sqlite3");
+const { Client } = require("pg");
+
+var config = parse(
+  "postgres://someuser:somepassword@somehost:381/somedatabase"
+);
+
+const client = new Client({ connectionString: connectionString });
+client.connect();
+
+client.query(
+  "CREATE TABLE people (id SERIAL, name TEXT, url TEXT, twitter TEXT, github TEXT, AvatarUrl TEXT, location TEXT, email TEXT, producthunt_id TEXT UNIQUE, PRIMARY KEY(id, producthunt_id) );",
+  (err, res) => {
+    console.log(err, res);
+  }
+);
+client.query("CREATE TABLE votes (uid TEXT, pid INT)", (err, res) => {
+  console.log(err, res);
+});
+client.query(
+  "CREATE TABLE posts(id SERIAL, slug TEXT UNIQUE, cursor TEXT, PRIMARY KEY (id, slug))",
+  (err, res) => {
+    console.log(err, res);
+    client.end();
+  }
+);
+
+/*SQLITE*/
+/*const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("montage.db");
 db.serialize(() => {
   //customers table
@@ -16,4 +43,4 @@ db.serialize(() => {
   console.log("successfully created the tables");
 });
 
-db.close();
+db.close();*/
--- server.js
+++ server.js
@@ -2,53 +2,64 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
-const sqlite3 = require("sqlite3");
-const db = new sqlite3.Database("montage.db");
+const { Client } = require("pg");
+const escape = require("pg-escape");
+var config = parse(
+  "postgres://someuser:somepassword@somehost:381/somedatabase"
+);
 
+const client = new Client({ connectionString: connectionString });
+client.connect();
 function replaceAll(str, find, replace) {
   return str.replace(new RegExp(find, "g"), replace);
 }
 
 app.get("/user/:id", (req, res) => {
   if (req.params.id == "initialdefault-firstuser") {
-    var sql = "SELECT * FROM people where id = 1";
+    var sql = "SELECT * FROM people LIMIT 1";
   } else {
-    var sql = 'SELECT * FROM people where id="' + req.params.id + '"';
+    var sql = escape(
+      "SELECT * FROM people where producthunt_id= %L",
+      req.params.id
+    );
   }
 
-  db.all(sql, (err, rows) => {
-    console.log(rows);
-    console.log(req.params.id);
-    res.send(rows);
-    console.log("1111111");
-    console.log(db);
+  let resData;
+  client.query(sql, (error, response) => {
+    //console.log(err, res);
+    resData = response.rows[0];
+    console.log(resData);
+    res.send(resData);
   });
 });
 
 app.get("/users", (req, res) => {
   var sql = "SELECT * FROM people";
-  db.all(sql, (err, rows) => {
-    console.log(rows);
-    console.log(req.params.id);
-    res.send(rows);
-    console.log("1111111");
+  let resData;
+  client.query(sql, (error, response) => {
+    //console.log(err, res);
+    resData = response.rows;
+    console.log(resData);
+    res.send(resData);
   });
 });
 
 app.get("/users/:posts", (req, res) => {
-  const filters = req.params.posts.split(",");
-  console.log(filters);
+  const filters = req.params.posts.split("&");
   let sqlfilter = filters.map(e => {
-    return "or title = '" + e + "'";
+    return "'" + e + "'";
   });
-  var sql =
-    'select * from people where id in (select uid from votes where pid in(select id from posts where title = ""' +
-    sqlfilter +
-    "))";
-  sql = replaceAll(sql, ",", "");
-  db.all(sql, (err, rows) => {
-    console.log(rows);
-    res.send(rows);
+  var sql = escape(
+    "select * from people where producthunt_id in (select uid from votes where pid in (SELECT id FROM posts WHERE slug in (%s)));",
+    sqlfilter.toString()
+  );
+
+  let resData;
+  client.query(sql, (error, response) => {
+    //console.log(err, res);
+    resData = response.rows;
+    console.log(resData);
+    res.send(resData);
   });
 });
 
